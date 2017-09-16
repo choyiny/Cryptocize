0be2755ac3d6ccfc1136b93394ca1c095d637028
lainiew <elaine.wu.ew@gmail.com>
2017-09-16 04:27:06 -0400

    Nav Button
    Prepping connection between Nav and Fragments

--- app/src/main/java/app/cryptocize/com/cryptocize/MainActivity.java
+++ app/src/main/java/app/cryptocize/com/cryptocize/MainActivity.java
@@ -1,5 +1,7 @@
 package app.cryptocize.com.cryptocize;
 
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -16,15 +18,34 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+          FragmentManager fm = getFragmentManager();
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                  fm.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                  fm.beginTransaction()
+                      .replace(R.id.content_main2
+                          , Fragment)
+                      .addToBackStack("string")
+                      .commit();
+                    //mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                  fm.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                  fm.beginTransaction()
+                      .replace(R.id.content_main2
+                          , Frag2)
+                      .addToBackStack("string")
+                      .commit();
+                    //mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                  fm.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                  fm.beginTransaction()
+                      .replace(R.id.content_main2
+                          , Frag3)
+                      .addToBackStack("string")
+                      .commit();
+                    //mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -41,5 +62,6 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 
 }
